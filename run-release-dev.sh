f2d675fd997e61afbee10f12be6150870f9dba60
Tang Bo Hao <tech@btang.cn>
2019-09-22 02:35:58 +0800

    fix sh

--- run-release-dev.sh
+++ run-release-dev.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ -s ./target/release/castor ] then
+if [ -s ./target/release/castor ]; then
   ./target/release/castor purge-chain --dev -y
 else
   cargo run --release -- purge-chain --dev -y
